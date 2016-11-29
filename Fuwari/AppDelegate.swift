5eda2d357240cd808fcad20992a5f40d447c8dcb
Kengo Yokoyama <rbken.lo.ovl@gmail.com>
2016-11-29 18:48:25 +0900

    :heavy_plus_sign: Add project

---
+++ Fuwari/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Fuwari
+//
+//  Created by Kengo Yokoyama on 2016/11/29.
+//  Copyright © 2016年 AppKnop. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Fuwari/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Fuwari
+//
+//  Created by Kengo Yokoyama on 2016/11/29.
+//  Copyright © 2016年 AppKnop. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
