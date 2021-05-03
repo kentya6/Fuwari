fda5bcba5ce1b3d5b70ade41add14a2ad419c530
Kengo Yokoyama <appknop@gmail.com>
2021-05-03 17:48:32 +0900

    :heavy_plus_sign: Change default capture shortcut

--- Fuwari/HotKeyManager.swift
+++ Fuwari/HotKeyManager.swift
@@ -19,7 +19,7 @@ final class HotKeyManager: NSObject {
         if let keyCombo = self.defaults.archiveDataForKey(KeyCombo.self, key: Constants.UserDefaults.captureKeyCombo) {
             return keyCombo
         } else {
-            let defaultKeyCombo = KeyCombo(key: .m, cocoaModifiers: [.command, .shift])!
+            let defaultKeyCombo = KeyCombo(key: .seven, cocoaModifiers: [.command, .shift])!
             self.defaults.setArchiveData(defaultKeyCombo, forKey: Constants.UserDefaults.captureKeyCombo)
             self.defaults.synchronize()
             return defaultKeyCombo
